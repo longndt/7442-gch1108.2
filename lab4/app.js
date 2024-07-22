193c90aeeaaa1680e5a869490bde2787400d1a95
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-07-22 12:12:57 +0700

    lab4

---
+++ lab4/app.js
@@ -0,0 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+//import & config "mongoose" package : used to work with MongoDB
+const mongoose = require('mongoose')
+
+const db_local = "mongodb://localhost:27017"   // const db_local = "mongodb://127.0.0.1:27017"
+const db_cloud = "mongodb+srv://longndt:xdHB48B3Fv*%40TPa@my-cluster.hzyaslz.mongodb.net/"
+
+mongoose.connect(db_local)
+  .then(() => console.log('connect to db succeed !'))
+.catch((err) => console.error('connect to db failed ! ' + err))
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
